b75b208f3895a41bd16408323592a003b9aec2fb
eduardo-siq <g.s.edu@hotmail.com>
2018-08-27 16:18:52 -0300

    Glow
    Adicionado um brilho ao avatar no menu. O brilho é feito com um GameObject com RawImage abaixo do avatar, controlado por um script (GlowScript) que compões o GameObject.

---
+++ Unity Projects/SuperQuiz/Assets/Scripts/GlowScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlowScript : MonoBehaviour  {
+	
+	RectTransform rect;
+	Vector2 baseSize;
+	float angle;
+	
+	void Start(){
+		rect = this.gameObject.GetComponent<RectTransform>();
+		baseSize = rect.sizeDelta;
+	}
+	
+	void Update(){
+		angle = angle + Time.deltaTime;
+		rect.sizeDelta = new Vector2 (baseSize.x + 5 * Mathf.Cos(angle), baseSize.y + 5 * Mathf.Cos(angle));
+	}
+	
+	
+}
+
